ad0f227e529e30e21cfb4861e4dc5b7c9e851a5d
A_ito <BoxPistols@users.noreply.github.com>
2022-11-05 07:18:15 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "asagiri";
+import "./styles.css";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setUsers(data));
+  }, []);
+  console.log(users);
+  /**
+   * id: 1
+   * name: "Leanne Graham"
+   * username: "Bret"
+   * email: "Sincere@april.biz"
+   * address: Object
+   * phone: "1-770-736-8031 x56442"
+   * website: "hildegard.org"
+   * company: Object
+   */
+
+  return (
+    <div className="App">
+      <h1>Search App</h1>
+      <label>Input</label>
+      <input type="text" placeholder="...input" />
+      {/* content */}
+      <div className="content">
+        <div className="section ">
+          {/* Box */}
+          {users.map((user, index) => (
+            <div className="box" key={index}>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </div>
+          ))}
+          {/* Box 2 */}
+          <div className="box">
+            <h2>User Name</h2>
+            <hr />
+            <p>Mail</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
