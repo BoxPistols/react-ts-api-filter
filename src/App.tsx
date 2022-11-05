aa99d29098e41eaf2ef2c8d5344da4dde91c6ed7
A_ito <BoxPistols@users.noreply.github.com>
2022-11-06 06:55:01 +0900

    move Base

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import "./styles.css";
 
 export default function App() {
   const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState([users]);
+
+  // const SearchQuery
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -13,23 +16,23 @@ export default function App() {
   }, []);
   // console.log(users);
 
-  /**
-   * id: 1
-   * name: "Leanne Graham"
-   * username: "Bret"
-   * email: "Sincere@april.biz"
-   * address: Object
-   * phone: "1-770-736-8031 x56442"
-   * website: "hildegard.org"
-   * company: Object
-   */
   type NewType = LegacyRef<any> | undefined;
   const ref: NewType = useRef();
   // console.log(ref);
 
   const handleSearch = () => {
     console.log(ref.current.value);
+
+    setSearchQuery(
+      users.filter((user) =>
+        user.name.toLowerCase().includes(ref.current.value.toLowerCase())
+      )
+    );
   };
+
+  // filter
+  // const handleSearch = () => {
+  // };
 
   return (
     <div className="App">
@@ -44,21 +47,26 @@ export default function App() {
       {/* content */}
       <div className="content">
         <div className="section ">
-          {/* Box */}
-          {users.map((user, index) => (
+          {/* {(searchQuery.length <= 0 ? users : searchQuery).map( */}
+          {searchQuery.map((user, index) => (
             <div className="box" key={index}>
               <h2>{user.name}</h2>
               <p>{user.email}</p>
             </div>
           ))}
-          {/* Box 2 */}
-          <div className="box">
-            <h2>User Name</h2>
-            <hr />
-            <p>Mail</p>
-          </div>
         </div>
       </div>
     </div>
   );
 }
+
+/**
+ * id: 1
+ * name: "Leanne Graham"
+ * username: "Bret"
+ * email: "Sincere@april.biz"
+ * address: Object
+ * phone: "1-770-736-8031 x56442"
+ * website: "hildegard.org"
+ * company: Object
+ */
